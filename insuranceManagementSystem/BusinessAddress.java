313ec39039a360da0dbc95aee9eafaa7642ffe28
Ali Yılmaz <123991935+aliylmztr@users.noreply.github.com>
2023-07-10 23:45:53 +0300

    Update BusinessAddress.java

--- insuranceManagementSystem/BusinessAddress.java
+++ insuranceManagementSystem/BusinessAddress.java
@@ -4,5 +4,4 @@ public class BusinessAddress extends Address {
     public BusinessAddress(String country, String city, String town, String neighborhood, String street) {
         super(country, city, town, neighborhood, street);
     }
-
 }
