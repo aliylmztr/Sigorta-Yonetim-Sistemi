af552d80d9f92eaaddb92523a62b05798e138da9
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-07-10 23:44:39 +0300

    Sigorta-Yonetim-Sistemi

---
+++ insuranceManagementSystem/BusinessAddress.java
@@ -0,0 +1,8 @@
+package insuranceManagementSystem;
+
+public class BusinessAddress extends Address {
+    public BusinessAddress(String country, String city, String town, String neighborhood, String street) {
+        super(country, city, town, neighborhood, street);
+    }
+
+}
---
+++ insuranceManagementSystem/Main.java
@@ -0,0 +1,32 @@
+package insuranceManagementSystem;
+
+public class Main {
+    public static void main(String[] args) throws InvalidAuthenticationException {
+
+        AccountManager accountManager = new AccountManager();
+
+        User user1 = new User("Ali", "YILMAZ", "aliyilmaztr96@gmail.com", "123456", "Yazılım Geliştiricisi", 27);
+
+        user1.getInsurances().add(new CarInsurance(100000));
+        user1.getInsurances().add(new HealthInsurance(75000));
+        user1.getInsurances().add(new ResidenceInsurance(250000));
+        user1.getInsurances().add(new TravelInsurance(50000));
+
+
+        AddressManager addressManager = new AddressManager(user1);
+        addressManager.addAddress((new HomeAddress("Türkiye","İstanbul","Kartal", "A","B")));
+        addressManager.addAddress((new BusinessAddress("Türkiye","Kocaeli","Gebze", "C","D")));
+
+        Account account1 = new Individual(user1);
+
+        accountManager.getDataList().add(account1);
+
+        accountManager.login("aliyilmaztr96@gmail.com", "123456");
+
+    }
+}
+class InvalidAuthenticationException extends Exception {
+    public InvalidAuthenticationException(String message) {
+        super(message);
+    }
+}
